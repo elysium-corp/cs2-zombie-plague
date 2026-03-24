c1665456bf06a88a920ce3084cfb092937407c70
fdrinv <fedorinovea@gmail.com>
2026-03-24 16:19:12 +0700

    [REFACTOR] updated

---
+++ ZPCore/src/Data/Zombies/IZombie.cs
@@ -0,0 +1,13 @@
+﻿using SwiftlyS2.Shared.Players;
+using ZPCore.Data.Zombies.ZClasses;
+
+namespace ZPCore.Data.Zombies;
+
+internal interface IZombie
+{
+    IPlayer Player { get; }
+    
+    IZClass ZClass { get; }
+    
+    public bool IsNemesis { get; }
+}
