c1387cd6e8983e7e4f6d5868685fd094d18daa68
fdrinv <fedorinovea@gmail.com>
2026-08-19 23:55:52 +0700

    added migration files

---
+++ Admin.Core/Database/Migrations/20260819165250_CreatePlayerPrivileges.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Admin.Core.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatePlayerPrivileges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "admin");
+
+            migrationBuilder.CreateTable(
+                name: "player_privileges",
+                schema: "admin",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    steam_id = table.Column<long>(type: "bigint", nullable: false),
+                    privilege_key = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    expires_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_player_privileges", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ux_player_privileges_steam_privilege",
+                schema: "admin",
+                table: "player_privileges",
+                columns: new[] { "steam_id", "privilege_key" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "player_privileges",
+                schema: "admin");
+        }
+    }
+}
