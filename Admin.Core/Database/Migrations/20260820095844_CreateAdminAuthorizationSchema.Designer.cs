230fb3783f4a15dfe9e5b933821357ea248d581d
fdrinv <fedorinovea@gmail.com>
2026-08-20 19:38:59 +0700

    migration update

--- Admin.Core/Database/Migrations/20260820095844_CreateAdminAuthorizationSchema.Designer.cs
+++ Admin.Core/Database/Migrations/20260820095844_CreateAdminAuthorizationSchema.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Admin.Core.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Admin.Core.Database.Migrations
 {
     [DbContext(typeof(AdminDbContext))]
-    partial class AdminDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260820095844_CreateAdminAuthorizationSchema")]
+    partial class CreateAdminAuthorizationSchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
