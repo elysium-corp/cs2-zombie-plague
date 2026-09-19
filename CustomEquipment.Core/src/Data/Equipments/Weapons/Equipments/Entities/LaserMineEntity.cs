0b0a42cdb978125ee90563d81446c727295f8e85
Evgeny Fedorinov <fedorinovea@gmail.com>
2026-09-19 23:42:38 +0700

    fix(laser-mine): stop ticks for invalid owners

--- CustomEquipment.Core/src/Data/Equipments/Weapons/Equipments/Entities/LaserMineEntity.cs
+++ CustomEquipment.Core/src/Data/Equipments/Weapons/Equipments/Entities/LaserMineEntity.cs
@@ -59,7 +59,8 @@ public sealed class LaserMineEntity : LaserMineEntityBase
             return;
         }
 
-        if (Owner == null)
+        var owner = Owner;
+        if (owner is not { IsValid: true })
         {
             Destroy();
             return;
@@ -71,7 +72,7 @@ public sealed class LaserMineEntity : LaserMineEntityBase
 
         if (!foundTarget) return;
 
-        ApplyDamage(target);
+        ApplyDamage(target, owner);
 
         // CreateDamageParticle(hitPoint);
     }
@@ -118,10 +119,10 @@ public sealed class LaserMineEntity : LaserMineEntityBase
         return true;
     }
 
-    private void ApplyDamage(IPlayer target)
+    private void ApplyDamage(IPlayer target, IPlayer owner)
     {
         var targetPawn = target.PlayerPawn;
-        var ownerPawn = Owner?.PlayerPawn;
+        var ownerPawn = owner.PlayerPawn;
         var mine = LaserMine;
 
         if (targetPawn is not { IsValid: true } ||
